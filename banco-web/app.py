ff40a7ffd803b8a2d4b715196193bf076f6eaab3
herdes <soaressantos02000@gmail.com>
2026-01-28 18:36:42 -0300

    função deposito na web

--- banco-web/app.py
+++ banco-web/app.py
@@ -1,24 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import models
 
 app = Flask(__name__)
+
+saldo = 0
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+LIMITE = 500
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/deposito", methods=["GET" ,"POST"])
+def deposito():
+    global saldo, extrato
+
+    mensagem = None
+
+    if request.method == "POST":
+        valor = float(request.form["valor"])
+        saldo, extrato, mensagem = models.depositar(saldo, valor, extrato)
+
+    return render_template("deposito.html", saldo=saldo, mensagem=mensagem)
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-# dados = [
-#     LIMITE_SAQUES = 3,
-#     AGENCIA = '0001',
-
-#     saldo = 0,
-#     limite = 500,
-#     extrato = '',
-#     numero_saques = 0,
-#     usuarios = [],
-#     contas = [],
-#     numero_conta = 1
-# ]
